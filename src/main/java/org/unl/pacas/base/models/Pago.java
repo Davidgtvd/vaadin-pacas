328739beb477324c6004133aac6e2bbd6661e279
JimmyOntaneda <jimmy.ontaneda@unl.edu.ec>
2025-06-15 20:27:10 -0500

    Pago beta

---
+++ src/main/java/org/unl/pacas/base/models/Pago.java
@@ -0,0 +1,24 @@
+package org.unl.pacas.base.models;
+
+import java.time.LocalDateTime;
+
+
+public class Pago {
+    private Long id;
+    private String codigoSeguridad;
+    private MetodoPago metodoPago;
+    private Boolean estado;
+    private LocalDateTime fechaPago;
+    
+    // Getters y Setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getCodigoSeguridad() { return codigoSeguridad; }
+    public void setCodigoSeguridad(String codigoSeguridad) { this.codigoSeguridad = codigoSeguridad; }
+    public MetodoPago getMetodoPago() { return metodoPago; }
+    public void setMetodoPago(MetodoPago metodoPago) { this.metodoPago = metodoPago; }
+    public Boolean getEstado() { return estado; }
+    public void setEstado(Boolean estado) { this.estado = estado; }
+    public LocalDateTime getFechaPago() { return fechaPago; }
+    public void setFechaPago(LocalDateTime fechaPago) { this.fechaPago = fechaPago; }
+}
